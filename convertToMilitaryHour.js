284d342a80e945f6895ce0575e10ae957bff5e18
IrvingGlt <41974385+Krpeta21@users.noreply.github.com>
2023-03-16 22:38:45 -0600

    🐛 fix: correccion

--- convertToMilitaryHour.js
+++ convertToMilitaryHour.js
@@ -5,12 +5,15 @@ function convertToMilitary(hour){
         splitedHour[2] = splitedHour[2].substring(0,2)
     }
     if(splitedHour[2].includes('AM')){
+        if(splitedHour[0] === '12'){
+            splitedHour[0] = '00'
+        }
         removeLettersFromTime()
         convertedHour = splitedHour.join(':')
     }else{
         removeLettersFromTime()
         if(splitedHour[0] === '12'){
-            splitedHour[0] = '00'
+            splitedHour[0] = '12'
         }else{
             splitedHour[0] =  parseInt(splitedHour[0]) + 12
         }
@@ -19,4 +22,4 @@ function convertToMilitary(hour){
      return convertedHour
 }
 
-console.log(convertToMilitary('11:05:45PM'))
+console.log(convertToMilitary('12:05:45AM'))
